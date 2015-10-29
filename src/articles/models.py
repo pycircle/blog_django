d5e3a03f65ea73456c0917a7b54d092459ab6c1e
Prusayn <prusayn@PrusBook.local>
2015-10-29 09:29:53 +0100

    Author added in models

--- src/articles/models.py
+++ src/articles/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.utils.text import slugify
+from django.conf import settings
 from unidecode import unidecode
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
 
 class Article(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False, unique=True)
     short_description = models.CharField(max_length=500, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True, null=False, blank=True)
-
+    author = models.ForeignKey(AUTH_USER_MODEL)
     published = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, null=False, blank=True)
 
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-
 
     def __unicode__(self):
         return self.title
