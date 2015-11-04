c0fcb6875be53b70c3ef6feae3bc25ab7ae680b7
Prusayn <prusayn@prusbook.lan>
2015-11-05 00:21:05 +0100

    List display options added

--- src/articles/admin.py
+++ src/articles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published', 'author', 'timestamp']
+
+admin.site.register(Article, ArticleAdmin)
