b5e8aefcd54761ff026236d50399541f499b975b
Prusayn <prusayn@PrusBook.local>
2015-12-09 18:00:20 +0100

    Fixed app config

--- src/blog/__init__.py
+++ src/blog/__init__.py
@@ -1,1 +1,1 @@
-default_app_config = 'articles.apps.ArticlesConfig'
+default_app_config = 'blog.apps.BlogConfig'
--- src/blog/apps.py
+++ src/blog/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class ArticlesConfig(AppConfig):
+class BlogConfig(AppConfig):
     name = 'blog'
     verbose_name = _("Blog")
