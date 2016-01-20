63d60ee122fff7f57e2d4fe52ff2c8bb6d390847
Rayvenden <rayunderwater@gmail.com>
2016-01-20 15:32:15 +0100

    Add UserAbout model with admin view

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Article, Category
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Article, Category, UserAbout
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -10,5 +12,17 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'parent']
     prepopulated_fields = {"slug": ("name",)}
 
+class UserAboutInline(admin.StackedInline):
+    model = UserAbout
+    can_delete = False
+    verbose_name_plural = 'About User'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserAboutInline, )
+
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+# re-register user admin with new fields
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
